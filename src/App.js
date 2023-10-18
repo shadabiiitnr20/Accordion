22367a28dd142a178667fdb6d4cb9e593639e965
Md. Shadab Hussain <shadab.iiitnr20@gmail.com>
2023-10-18 12:34:07 +0530

    Accordion Implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { list } from "./constants/accordionData";
 
 function App() {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const toggle = (id) => {
+    if (selectedItem === id) {
+      return setSelectedItem(null);
+    }
+    setSelectedItem(id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className="text-2xl font-bold underline text-yellow-400 m-2 p-1">
+        My Accordion
+      </h2>
+      <div>
+        {list.map((item) => {
+          return (
+            <div
+              className="border border-solid border-black m-2 bg-yellow-100"
+              key={item.id}
+            >
+              <div
+                className="font-semibold mx-1 my-0.5 p-0.5 text-red-500 flex justify-between items-center"
+                onClick={() => toggle(item.id)}
+              >
+                {item.question}
+                <span className="text-2xl my-0.5">
+                  {selectedItem === item.id ? "-" : "+"}
+                </span>
+              </div>
+              <div
+                className={`font-normal mx-1 p-0.5 ${
+                  selectedItem === item.id ? "block" : "hidden"
+                }`}
+              >
+                {item.answer}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
